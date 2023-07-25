3de8c40af49bc12a6d85f93610628b36e3de50ea
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-07-25 22:05:44 +0000

    add homeroute and show page

--- app.rb
+++ app.rb
@@ -3,8 +3,5 @@ require "sinatra/reloader"
 require "sinatra/activerecord"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:cat_show)
 end
