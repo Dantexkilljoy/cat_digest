afc8d7610b4e55f7b7f118b7c42f49915e6208ba
Dustin Doyle <126806607+Dantexkilljoy@users.noreply.github.com>
2023-07-27 15:57:42 +0000

    small format to app.rb

--- app.rb
+++ app.rb
@@ -2,11 +2,9 @@ require "sinatra"
 require "sinatra/reloader"
 require "./services/cats"
 
-
 get("/") do
   @cat_lookup = Cats.lookup("url")
 
   @cat_quotes = ["~Meow Meow Meow~", "nya~nya~", "HISSss", "Mrreyaaoooo", "mew"]
-
   erb(:root)
 end
